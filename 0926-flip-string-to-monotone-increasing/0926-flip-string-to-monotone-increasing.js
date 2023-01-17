26573bf5d5990bc1e8372788b0323a938abf9677
vetan2 <vetan2@naver.com>
2023-01-17 10:39:27 +0900

    Time: 148 ms (18.18%), Space: 48.4 MB (29.55%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.js
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.js
@@ -4,22 +4,16 @@
  */
 
 var minFlipsMonoIncr = function(s) {
-    const flipsToMakeAllOne = [];
-    let temp
+    let result = 0;
+    let oneCount = 0;
 
-    temp = 0;
-    for(let i = s.length - 1; i >= 0; i--) {
-        if(s[i] === '0') ++temp;
-        flipsToMakeAllOne[i] = temp;
-    }
-    flipsToMakeAllOne[s.length] = 0;
-    
-    let result = 987654321;
-    temp = 0;
-    for(let i = -1; i < s.length; i++) {
-        if(s[i] === '1') ++temp;
-        result = Math.min(result, temp + flipsToMakeAllOne[i+1])
-    }
+    Array.from({length: s.length}).forEach((_, i) => {
+        if(s[i] === '0') {
+            result = Math.min(oneCount, result + 1);
+        } else {
+            oneCount++;
+        }
+    });
 
     return result;
 };
