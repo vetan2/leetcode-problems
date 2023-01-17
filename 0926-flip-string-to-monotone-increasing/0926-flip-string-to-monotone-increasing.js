ef538ce4e963a9737886f7baf2d2b8946b584bc6
vetan2 <vetan2@naver.com>
2023-01-17 10:25:54 +0900

    Time: 164 ms (11.36%), Space: 51.6 MB (20.45%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.js
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.js
@@ -4,24 +4,21 @@
  */
 
 var minFlipsMonoIncr = function(s) {
-    const flipsToMakeAllZero = {};
-    const flipsToMakeAllOne = {};
-    let temp1, temp0;
+    const flipsToMakeAllOne = [];
+    let temp
 
-    temp1 = 0;
-    temp0 = 0;
-    for(let i = 0; i < s.length ; i++) {
-        if(s[i] === '1') ++temp0;
-        if(s[s.length - i - 1] === '0') ++temp1;
-        flipsToMakeAllZero[i] = temp0;
-        flipsToMakeAllOne[s.length - i -1] = temp1;
+    temp = 0;
+    for(let i = s.length - 1; i >= 0; i--) {
+        if(s[i] === '0') ++temp;
+        flipsToMakeAllOne[i] = temp;
     }
-    flipsToMakeAllZero[-1] = 0;
     flipsToMakeAllOne[s.length] = 0;
-
+    
     let result = 987654321;
-    for(let i = -1; i < s.length; i++) { // 0 기준
-        result = Math.min(result, flipsToMakeAllZero[i] + flipsToMakeAllOne[i+1]);
+    temp = 0;
+    for(let i = -1; i < s.length; i++) {
+        if(s[i] === '1') ++temp;
+        result = Math.min(result, temp + flipsToMakeAllOne[i+1])
     }
 
     return result;
