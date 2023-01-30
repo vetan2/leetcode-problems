ff8c8303dd8ef63a5dd46b08e0de059ca2066f22
vetan2 <vetan2@naver.com>
2023-01-30 18:43:03 +0900

    Time: 50 ms (99.30%), Space: 41.8 MB (63.27%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var tribonacci = function(n) {
+    const arr = [0,1,1];
+    if(n <= 2) {
+        return arr[n];
+    }
+
+    let head = 2;
+    let sum = 2;
+    while(head < n) {
+        arr[head + 1] = sum;
+        sum = sum - arr[head - 2] + arr[head + 1];
+        head++;
+    }
+
+    return arr[n];
+};
