ca84eb9aacce328f9cf19fae423ed0e1ecf3c2b0
vetan2 <vetan2@naver.com>
2023-03-17 00:56:46 +0900

    Time: 82 ms (80.08%), Space: 45.4 MB (90.68%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.js
+++ 0006-zigzag-conversion/0006-zigzag-conversion.js
@@ -3,25 +3,25 @@
  * @param {number} numRows
  * @return {string}
  */
-var convert = function(s, numRows) {
-    if(numRows === 1 || numRows > s.length) {
-        return s;
+var convert = function (s, numRows) {
+  if (numRows === 1 || numRows > s.length) {
+    return s
+  }
+
+  const resArr = new Array(numRows).fill("")
+
+  let idx = 0
+  let step = 1
+  for (const str of s) {
+    resArr[idx] += str
+    if (idx === 0) {
+      step = 1
+    } else if (idx === numRows - 1) {
+      step = -1
     }
 
-    const resArr = new Array(numRows).fill('');
+    idx += step
+  }
 
-    let idx = 0;
-    let step = 1;
-    for(const str of s) {
-        resArr[idx] += str;
-        if(idx === 0) {
-            step = 1;
-        } else if (idx === numRows - 1) {
-            step = -1;
-        }
-
-        idx += step;
-    }
-
-    return resArr.join('');
-};
+  return resArr.join("")
+}
